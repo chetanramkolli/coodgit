1e48c77b6e3d098b1d8a57179c8abc2bdeac4f9c
K.Chetan Ram <123228820+chetanramkolli@users.noreply.github.com>
2023-05-29 23:29:23 +0530

    Create j2.js

---
+++ j2.js
@@ -0,0 +1,15 @@
+1.Create an array called "fruits" with the following fruits: "apple", "banana", "orange", "grape", "mango".
+code:
+let fruits = [];
+fruits.push("apple");
+fruits.push("banana");
+fruits.push("orange");
+fruits.push("grape");
+fruits.push("mango");
+
+
+2.Use JavaScript to display the total number of fruits in the array
+code:
+const fruits = ["apple", "banana", "orange", "grape", "mango"];
+const numberOfFruits = fruits.length;
+console.log(numberOfFruits);
